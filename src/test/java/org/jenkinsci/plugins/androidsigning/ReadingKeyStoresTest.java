8a3a834b45427a42c19ae579c2a82f4ca2e63964
Michael Trimarchi <michael@amarulasolutions.com>
2025-06-08 21:01:12 +0200

    src: Drop deprecated API
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

--- src/test/java/org/jenkinsci/plugins/androidsigning/ReadingKeyStoresTest.java
+++ src/test/java/org/jenkinsci/plugins/androidsigning/ReadingKeyStoresTest.java
@@ -21,7 +21,7 @@ import java.util.Base64;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class ReadingKeyStoresTest {
